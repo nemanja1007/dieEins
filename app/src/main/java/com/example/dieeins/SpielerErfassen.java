24bc45f7da102faf40f61c7697e6fdd43714b0e3
jahi-im-him <yancyj@bzz.ch>
2021-11-16 16:00:31 +0100

    initial commit Spieler erfassen

--- app/src/main/java/com/example/dieeins/SpielerErfassen.java
+++ app/src/main/java/com/example/dieeins/SpielerErfassen.java
@@ -4,17 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-public class MainActivity extends AppCompatActivity {
+public class SpielerErfassen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btn = (Button)findViewById(R.id.button2);
+        setContentView(R.layout.activity_spielererfassen);
+        Button btn = (Button)findViewById(R.id.button3);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SpielerErfassen.class));
+                startActivity(new Intent(SpielerErfassen.this, MainActivity.class));
             }
         });
     }
