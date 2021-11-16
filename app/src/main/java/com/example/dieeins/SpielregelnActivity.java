81a04c432b3f1586e034890c4f3e4e94a76a096b
nemanja1007 <petrovicn@bzz.ch>
2021-11-16 15:59:40 +0100

    Files für Spielregeln, CSS fehlt

--- app/src/main/java/com/example/dieeins/SpielregelnActivity.java
+++ app/src/main/java/com/example/dieeins/SpielregelnActivity.java
@@ -1,25 +1,24 @@
 package com.example.dieeins;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class SpielregelnActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_spielregeln);
 
-        Button btn = (Button)findViewById(R.id.button);
+        Button btn = (Button)findViewById(R.id.button3);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SpielregelnActivity.class));
+                startActivity(new Intent(SpielregelnActivity.this, MainActivity.class));
             }
         });
     }
