1ba4ad63effa1f5d298045cff83bceac6bb6a405
Jahi yancy <yancyj@bzz.ch>
2021-11-17 09:41:49 +0100

    merged

--- app/src/main/java/com/example/dieeins/MainActivity.java
+++ app/src/main/java/com/example/dieeins/MainActivity.java
@@ -10,11 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = (Button)findViewById(R.id.button2);
-        btn.setOnClickListener(new View.OnClickListener() {
+
+        Button btn2 = (Button)findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, SpielerErfassen.class));
+            }
+        });
+
+        Button btn= (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SpielregelnActivity.class));
             }
         });
     }
