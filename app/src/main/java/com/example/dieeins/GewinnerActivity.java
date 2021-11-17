6f0e37ec8860037f55ce0c5017ec957ea2f5a395
Jahi yancy <yancyj@bzz.ch>
2021-11-17 14:39:59 +0100

    Merge database & new files

--- app/src/main/java/com/example/dieeins/GewinnerActivity.java
+++ app/src/main/java/com/example/dieeins/GewinnerActivity.java
@@ -1,20 +1,21 @@
 package com.example.dieeins;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-public class SpielerErfassen extends AppCompatActivity {
+public class GewinnerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spielererfassen);
-        Button btn = (Button)findViewById(R.id.buttonSpielerErfassen);
+        Button btn = (Button)findViewById(R.id.button4);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SpielerErfassen.this, SpielfeldActivity.class));
+                startActivity(new Intent(GewinnerActivity.this, MainActivity.class));
             }
         });
     }
